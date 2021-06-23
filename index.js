770a35891c895bd8b93319b748e120d77a273361
jopemachine <jopemachine@naver.com>
2021-06-23 12:39:54 +0900

    upd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const arvish = require('arvish');
+const encodeurl = require('encodeurl');
 
 (async function () {
   const data = await arvish.fetch('http://ac.search.naver.com/nx/ac', {
@@ -16,9 +17,13 @@ const arvish = require('arvish');
     maxAge: 86400000,
     transform: (resp) => {
       return resp.items[0].map((item) => {
+        const word = item[0];
+        const encodedUrl = encodeurl(word.split(' ').join('%20'));
         return {
-          title: item[0],
-          arg: item[0]
+          title: word,
+          arg: encodedUrl,
+          autocomplete: item[0],
+          quicklookurl: `http://search.naver.com/search.naver?ie=utf8&sm=stp_hty&where=se&query=${encodedUrl}`
         };
       });
     }
